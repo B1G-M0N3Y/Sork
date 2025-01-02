d5cd108a747084db03377453f2295d9e66afde38
Noah Kerner <noah.kerner@improving.com>
2025-01-02 13:39:32 -0600

    Our first commands

--- Program.cs
+++ Program.cs
@@ -1,4 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
 
-
-Console.WriteLine("Hello, World!");
+namespace Sork
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            do
+            {
+                Console.Write(">");
+                string input = Console.ReadLine();
+                input = input.ToLower();
+                input = input.Trim();
+                if (input == "lol")
+                {
+                    Console.WriteLine("You laugh out loud");
+                }
+                else if (input == "exit")
+                {
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("Unknown command");
+                }
+            } while (true);
+        }
+    }
+}
