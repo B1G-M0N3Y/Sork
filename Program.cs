0db087caf83317c7ab845768c1e7a963615c184a
Noah Kerner <noah.kerner@improving.com>
2025-01-03 14:00:21 -0600

    lesson 2

--- Program.cs
+++ Program.cs
@@ -6,37 +6,108 @@ namespace Sork
     {
         public static void Main(string[] args)
         {
+            ICommand lol = new LaughCommand();
+            ICommand exit = new ExitCommand();
+            ICommand dance = new DanceCommand();
+            ICommand sing = new SingCommand();
+            ICommand whistle = new WhistleCommand();
+            List<ICommand> commands = new List<ICommand> { lol, dance, sing, whistle, exit };
+            var result = new CommandResult { RequestExit = false, IsHandled = false };
+            var handled = false;
             do
             {
                 Console.Write(">");
                 string input = Console.ReadLine();
                 input = input.ToLower();
                 input = input.Trim();
-                if (input == "lol")
+                foreach (var command in commands)
                 {
-                    Console.WriteLine("You laugh out loud");
+                    if (command.Handles(input))
+                    {
+                        handled = true;
+                        result = command.Execute();
+                        if (result.RequestExit)
+                        {
+                            break;
+                        }
+                    }
                 }
-                else if (input == "dance")
-                {
-                    Console.WriteLine("Cha cha cha");
-                }
-                else if (input == "sing")
-                {
-                    Console.WriteLine("Play Freebird!");
-                }
-                else if (input == "whistle")
-                {
-                    Console.WriteLine("What a lovely tune");
-                }
-                else if (input == "exit")
+                if (result.RequestExit)
                 {
                     break;
                 }
-                else 
+                if (!handled)
                 {
                     Console.WriteLine("Unknown command");
                 }
             } while (true);
+        }
+    }
+
+    public class CommandResult
+    {
+        public bool RequestExit { get; set; }
+        public bool IsHandled { get; set; }
+    }
+
+    public interface ICommand
+    {
+        bool Handles(string userInput);
+        CommandResult Execute();
+    }
+
+    public class ExitCommand : ICommand
+    {
+        public bool Handles(string userInput) => userInput == "exit";
+
+        public CommandResult Execute() => new CommandResult { RequestExit = true, IsHandled = true };
+    }
+
+
+    public class LaughCommand : ICommand
+    {
+        public bool Handles(string userInput)
+        {
+            return userInput == "lol";
+        }
+
+        public CommandResult Execute()
+        {
+            Console.WriteLine("You laugh out loud");
+            return new CommandResult { RequestExit = false, IsHandled = true };
+        }
+    }
+
+    public class DanceCommand : ICommand
+    {
+        public bool Handles(string userInput) => userInput == "dance";
+
+        public CommandResult Execute()
+        {
+            Console.WriteLine("Cha cha cha");
+            return new CommandResult { RequestExit = false, IsHandled = true };
+        }
+    }
+
+    public class SingCommand : ICommand
+    {
+        public bool Handles(string userInput) => userInput == "sing";
+
+        public CommandResult Execute()
+        {
+            Console.WriteLine("Play Freebird!");
+            return new CommandResult { RequestExit = false, IsHandled = true };
+        }
+    }
+
+    public class WhistleCommand : ICommand
+    {
+        public bool Handles(string userInput) => userInput == "whistle";
+
+        public CommandResult Execute()
+        {
+            Console.WriteLine("What a lovely tune");
+            return new CommandResult { RequestExit = false, IsHandled = true };
         }
     }
 }
